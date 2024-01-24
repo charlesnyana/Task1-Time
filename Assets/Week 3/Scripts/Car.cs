38b91f20feddf4b638af3e03f82ac86e5b9dbb01
nyanasen <nyanasen@OA-S141-05>
2024-01-24 11:08:28 -0500

    Task 3 complete: car controller

--- Assets/Week 3/Scripts/Car.cs
+++ Assets/Week 3/Scripts/Car.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
+
+    public float steeringSpeed;
+    public float forwardSpeed;
+    public float maxSpeed;
+    Rigidbody2D rigidBody;
+
+    float steering;
+    float acceleration;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        steering = Input.GetAxis("Horizontal");
+        acceleration = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.AddTorque(-steeringSpeed * steering * Time.deltaTime);
+        Vector2 force = transform.up * acceleration * forwardSpeed * Time.deltaTime;
         
+        if (rigidBody.velocity.magnitude < maxSpeed)
+        {
+            rigidBody.AddForce(force);
+        }
+ 
     }
 }
